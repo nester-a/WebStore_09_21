c0580a8b6ba64b7a75aa00da5315583c22845a99
Aleksey <nester.a.l@yandex.ru>
2021-09-15 22:01:20 +0300

    Представление сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee() { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee() { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee() { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
         /// <summary>
         /// Действие данного контроллера будет доступно, если мы перейдёи по ссылке
         ///  http://localhost:5000/Home/Index
@@ -24,6 +32,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Second action with parameter {id}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }
