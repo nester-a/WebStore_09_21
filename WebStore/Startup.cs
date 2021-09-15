4294554affe9078e1d41c046895f0e2d9107ac1d
Aleksey <nester.a.l@yandex.ru>
2021-09-15 20:55:01 +0300

    Подключение конфигурации в классе Startup

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +9,21 @@ namespace WebStore
 {
     public class Startup
     {
+        /// <summary>
+        /// Данное свойство необходимо для того, чтобы мы могли
+        /// получить доступ к конфигурации в различных частях нашего
+        /// приложения.
+        /// </summary>
+        public IConfiguration Configuration { get; set; }
+
+        /// <summary>
+        /// Конструктор для внесения конфигурации
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
         }
