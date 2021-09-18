aa9c8eac4a308dfc6fa6081463cfb8cd5c246390
Aleksey <nester.a.l@yandex.ru>
2021-09-18 21:08:20 +0300

    Выделение работы с сотрудниками в отдельный контроллер

---
+++ WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee() { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
+            new Employee() { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
+            new Employee() { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+        public IActionResult Index() => View(_employees);
+    }
+}
--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,45 +9,11 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        private static readonly List<Employee> _employees = new()
-        {
-            new Employee() { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 27 },
-            new Employee() { Id = 2, LastName = "Петров", FirstName = "Пётр", Patronymic = "Петрович", Age = 31 },
-            new Employee() { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 18 },
-        };
-
         /// <summary>
         /// Действие данного контроллера будет доступно, если мы перейдёи по ссылке
         ///  http://localhost:5000/Home/Index
         /// </summary>
         /// <returns></returns>
-        public IActionResult Index()
-        {
-            //return Content("Hello from first controller");
-
-            //если у нас предаставление называется также как и действие
-            //то в парамерт метода ничего передавать не нужно
-            return View();
-        }
-        public IActionResult SecondAction(string id)
-        {
-            return Content($"Second action with parameter {id}");
-        }
-        public IActionResult Employees()
-        {
-            return View(_employees);
-        }
-
-        //принимаем сотрудника, находим про него инфу в базе и выводим
-        //хз почему, но не смог сделать так, чтобы просто принимало числовой ИДшник
-        public IActionResult Details(Employee employee)
-        {
-            var tmpEmployee = _employees.Find(empl => empl.Id == employee.Id);
-            if (tmpEmployee != null)
-            {
-                return View(tmpEmployee);
-            }
-            return Content("Сотрудник не найден");
-        }
+        public IActionResult Index() => View();
     }
 }
