07d505e27bb6aaba1635987ddfff9d3ac18233a8
Aleksey <nester.a.l@yandex.ru>
2021-09-25 15:16:54 +0300

    Реализация сервиса сотрудников. Для поверхностного копирования сотрудника реализован интерфейс ICloneable

---
+++ WebStore/Services/InMemoryEmployeesData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Data;
+using WebStore.Models;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly ILogger<InMemoryEmployeesData> _logger;
+        private int _currentMaxId;
+        public InMemoryEmployeesData(ILogger<InMemoryEmployeesData> logger)
+        {
+            _logger = logger;
+            _currentMaxId = TestData.Employees.Max(e => e.Id);
+        }
+        public int Add(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            if (TestData.Employees.Contains(employee)) return employee.Id;
+
+            employee.Id = ++_currentMaxId;
+            TestData.Employees.Add(employee);
+            return employee.Id;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_employee = GetById(id);
+            if (db_employee is null) return false;
+
+            TestData.Employees.Remove(db_employee);
+            return true;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return TestData.Employees;
+        }
+
+        public Employee GetById(int id)
+        {
+            return TestData.Employees.SingleOrDefault(e => e.Id == id);
+        }
+
+        public void Update(Employee employee)
+        {
+            if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+            //эта строка нужна только для реализации сервиса "в памяти"
+            if (TestData.Employees.Contains(employee)) return;
+
+            var db_employee = GetById(employee.Id);
+            if (db_employee is null) return;
+
+            db_employee = (Employee)employee.Clone();
+
+            //db.SaveChanges();
+        }
+    }
+}
