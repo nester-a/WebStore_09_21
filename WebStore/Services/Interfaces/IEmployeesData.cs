a1e276d04c675bced8ed7bd2107f299019593cdc
Aleksey <nester.a.l@yandex.ru>
2021-09-25 14:11:16 +0300

    Интерфейс сервиса сотрудников

---
+++ WebStore/Services/Interfaces/IEmployeesData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Models;
+
+namespace WebStore.Services.Interfaces
+{
+    //здесь интерфейс это то, как можно будет использовать
+    //наш сервис снаружи/извне
+    //для интерфейса сервиса пишуться исключительно методы (без свойств, событий и прочего)
+    public interface IEmployeesData
+    {
+        IEnumerable<Employee> GetAll();
+
+        Employee GetById(int id);
+
+        int Add(Employee employee);
+
+        void Update(Employee employee);
+
+        bool Delete(int id);
+    }
+}
