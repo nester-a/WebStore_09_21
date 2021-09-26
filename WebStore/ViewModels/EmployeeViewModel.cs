3be15fe9fea524a8471ebf20ea63ae09d2e4991e
Aleksey <nester.a.l@yandex.ru>
2021-09-26 16:19:16 +0300

    Визуальные аннотации модели-представления

--- WebStore/ViewModels/EmployeeViewModel.cs
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebStore.ViewModels
 {
     public class EmployeeViewModel
     {
+        [HiddenInput(DisplayValue = false)]
         public int Id { get; set; }
+
+        [Display(Name = "Имя")]
         public string Name { get; set; }
+
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
         public string SecondName { get; set; }
+
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
     }
 }
