a956be7decf7c36e4203e00c392d854c0b0d0b92
Aleksey <nester.a.l@yandex.ru>
2021-09-26 17:29:20 +0300

    Сервис каталога товаров + набор тестовых данных

---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();
+    }
+}
