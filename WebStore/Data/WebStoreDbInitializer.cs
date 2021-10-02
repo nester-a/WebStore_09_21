c784e2befc76cca6bea50643dc37ef5c3bb8cd96
Aleksey <nester.a.l@yandex.ru>
2021-10-02 16:47:28 +0300

    Инициализатор БД

---
+++ WebStore/Data/WebStoreDbInitializer.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+
+namespace WebStore.Data
+{
+    public class WebStoreDbInitializer
+    {
+        private readonly WebStoreDB _db;
+
+        public WebStoreDbInitializer(WebStoreDB db) => _db = db;
+
+        public async Task InitializeAsync()
+        {
+            //var db_deleted = await _db.Database.EnsureDeletedAsync();
+
+            //var db_created = await _db.Database.EnsureCreatedAsync();
+
+            var pending_migrations = await _db.Database.GetPendingMigrationsAsync();
+            var applied_migrations = await _db.Database.GetAppliedMigrationsAsync();
+            if (pending_migrations.Any())
+                await _db.Database.MigrateAsync();
+
+            await InitializeProductsAsync();
+        }
+
+        private async Task InitializeProductsAsync()
+        {
+            using (await _db.Database.BeginTransactionAsync())
+            {
+                _db.Sections.AddRange(TestData.Sections);
+
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Sections] OFF");
+                await _db.Database.CommitTransactionAsync();
+            }
+
+            using (await _db.Database.BeginTransactionAsync())
+            {
+                _db.Brands.AddRange(TestData.Brands);
+
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Brands] OFF");
+                await _db.Database.CommitTransactionAsync();
+            }
+
+            using (await _db.Database.BeginTransactionAsync())
+            {
+                _db.Products.AddRange(TestData.Products);
+
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] ON");
+                await _db.SaveChangesAsync();
+                await _db.Database.ExecuteSqlRawAsync("SET IDENTITY_INSERT [dbo].[Products] OFF");
+                await _db.Database.CommitTransactionAsync();
+            }
+        }
+    }
+}
