58ab096a1dc9998f508a08dfac34d24ccd5b2d38
Aleksey <nester.a.l@yandex.ru>
2021-10-05 21:57:04 +0300

    Регистрация нового пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels.Identity;
 
@@ -15,14 +16,36 @@ namespace WebStore.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+
+        #region Register
+
         public IActionResult Register() => View(new RegisterUserViewModel());
 
-        [HttpPost]
-        public IActionResult Register(RegisterUserViewModel model)
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
+            if (!ModelState.IsValid) return View(model);
 
-            return RedirectToAction("Index", "Home");
-        }
+            var user = new User
+            {
+                UserName = model.UserName,
+            };
+            var register_result = await _userManager.CreateAsync(user, model.Password);
+
+            if (register_result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+            foreach (var error in register_result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View(model);
+        } 
+
+        #endregion
+
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
         public IActionResult AccessDenied() => View();
--- WebStore/ViewModels/Identity/RegisterUserViewModel.cs
+++ WebStore/ViewModels/Identity/RegisterUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,18 @@ namespace WebStore.ViewModels.Identity
 {
     public class RegisterUserViewModel
     {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Подтверждение пароля")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
     }
 }
