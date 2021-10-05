9c678381aa31d1b35939e0c5be90d459e7856b2f
Aleksey <nester.a.l@yandex.ru>
2021-10-05 21:38:41 +0300

    Заготовка метода регистрации нового пользователя

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
@@ -14,7 +15,14 @@ namespace WebStore.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
-        public IActionResult Register() => View();
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
         public IActionResult AccessDenied() => View();
